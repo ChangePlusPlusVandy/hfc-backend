bea90c15037aa4f8bcd8bf25fd24a2d901996278
Raj Chopra <raj.chopra@vanderbilt.edu>
2022-11-08 16:24:17 -0600

    Add connection to Mongo and add more dependencies

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
+require('dotenv').config()
+
+// Connect to Mongo database
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Connected to Mongo! Success!");
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }  
+}
+
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('boo! 👻');
