7d3f7c6c532dd05f30278958bd7c508760bfd505
Raj Chopra <raj.chopra@vanderbilt.edu>
2022-11-29 19:32:10 -0600

    add cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const connectDB = require('./config/database');
 const app = express();
+const cors = require('cors')
 const PORT = 3000;
 
 
 connectDB();
 app.use(express.json())
+app.use(cors())
 app.use('/beneficiary',require('./routes/beneficiary'))
 app.use('/programs',require('./routes/programs'))
 
