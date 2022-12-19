34c77c9069e51c80d8a19bb1d91a31be1a95ca27
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2022-12-18 23:28:58 -0500

    Remove User and Beneficiary model files

--- models/workshops.models.js
+++ models/workshops.models.js
@@ -1,34 +1,36 @@
 const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema.Types;
+
 const WorkshopSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  hosts: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
+    title: {
+        type: String,
+        required: true,
+        unique: true,
     },
-  ],
-  description: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    required: true,
-  },
-  numAttendees: {
-    type: Number,
-    required: true,
-  },
-  attendees: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
+    hosts: [
+        {
+            type: ObjectId,
+            required: true,
+        },
+    ],
+    description: {
+        type: String,
+        required: true,
     },
-  ],
+    date: {
+        type: Date,
+        required: true,
+    },
+    numAttendees: {
+        type: Number,
+        required: true,
+    },
+    attendees: [
+        {
+            type: ObjectId,
+            required: true,
+        },
+    ],
 });
 
-module.exports = Workshop = mongoose.model("workShop", WorkshopSchema);
+module.exports = Workshop = mongoose.model("Workshop", WorkshopSchema);
