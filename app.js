671150822fcf8b990b4e3d03d621b5c407d43d7f
kai dubauskas <kaidubauskas@gmail.com>
2023-07-19 13:56:26 -0500

    test2

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const express = require("express");
-const cors = require("cors");
-const helmet = require("helmet");
-const connectDB = require("./config/database.js");
+// const express = require("express");
+// const cors = require("cors");
+// const helmet = require("helmet");
+// const connectDB = require("./config/database.js");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-connectDB(); // Connect to MongoDB
+// connectDB(); // Connect to MongoDB
 
 // Middleware
-app.use(express.json());
-app.use(cors());
-app.use(helmet());
+// app.use(express.json());
+// app.use(cors());
+// app.use(helmet());
 
-// Routers
-app.use("/assessments", require("./routes/assessments.router.js"));
-app.use("/beneficiaries", require("./routes/beneficiaries.router.js"));
-app.use("/programs", require("./routes/programs.router.js"));
-app.use("/users", require("./routes/users.router.js"));
-app.use("/workshops", require("./routes/workshops.router.js"));
+// // Routers
+// app.use("/assessments", require("./routes/assessments.router.js"));
+// app.use("/beneficiaries", require("./routes/beneficiaries.router.js"));
+// app.use("/programs", require("./routes/programs.router.js"));
+// app.use("/users", require("./routes/users.router.js"));
+// app.use("/workshops", require("./routes/workshops.router.js"));
 
 app.get('/', (req, res) => {
     res.send('boo!');
