97947fe15d061e5125e5d1e92e4adcab6f238aa2
kai dubauskas <kaidubauskas@gmail.com>
2023-07-18 21:20:48 -0500

    add vercel

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const helmet = require("helmet");
 const connectDB = require("./config/database.js");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
