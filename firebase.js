e8d5ef4e5ecc5dd8fb8a429ed8c7a4380325bb1f
kai dubauskas <kaidubauskas@gmail.com>
2023-09-05 16:09:59 -0500

    fix deployment issue

--- firebase.js
+++ firebase.js
@@ -8,13 +8,17 @@
 
 // module.exports = admin;
 // TODO: figure out the difference between above and below
+
+
+
+
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getAuth } = require("firebase-admin/auth");
 //const admin = require("firebase-admin");
 // const serviceAccount = require("./ServiceAccount.json");
 
 const app = initializeApp({
-    credential: cert(process.env.VERCEL_URI),
+    credential: cert(JSON.parse(process.env.VERCEL_URI)),
 });
 
 const auth = getAuth(app);
